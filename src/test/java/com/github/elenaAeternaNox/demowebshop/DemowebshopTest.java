f9f3e3673d4843def174baa21b3e3817c0327a9a
elenaAeternanox <antares87@mail.ru>
2021-12-28 21:26:18 +0100

    Demowebshop tests fix

--- src/test/java/com/github/elenaAeternaNox/demowebshop/DemowebshopTest.java
+++ src/test/java/com/github/elenaAeternaNox/demowebshop/DemowebshopTest.java
@@ -1,12 +1,12 @@
 package com.github.elenaAeternaNox.demowebshop;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Cookie;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
@@ -27,67 +27,71 @@ public class DemowebshopTest {
     @Tag("API")
     void checkWishListAPI() {
         body = "addtocart_53.EnteredQuantity=1";
-        step("Add product to Wishlist", () -> {given()
-                            .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                            .body(body)
-                            .when()
-                            .post("addproducttocart/details/53/2")
-                            .then()
-                            .statusCode(200)
-                            .body("updatetopwishlistsectionhtml", is("(1)"))
-                            .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
+        step("Add product to Wishlist", () -> {
+            given()
+                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                    .body(body)
+                    .when()
+                    .post("addproducttocart/details/53/2")
+                    .then()
+                    .statusCode(200)
+                    .body("updatetopwishlistsectionhtml", is("(1)"))
+                    .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
         });
     }
 
     @Test
     @Tags({@Tag("API"), @Tag("UI")})
-    void checkWishListOnUI() {
-        body = "addtocart_53.EnteredQuantity=2";
+    void checkUsersAddress() {
+        String login = "elena@qa.guru";
+        String password = "elena@qa.guru";
+        SelenideElement address = $(".address-list");
 
-        step("Get cookie by api and set it to browser", () -> {
-                    step("Get cookie and set it to browser by API", () -> {
-                        String authorizationCookie = given()
-                                .contentType("application/x-www-form-urlencoded")
-                                .formParam("Email", "elena@qa.guru")
-                                .formParam("Password", "elena@qa.guru")
-                                .when()
-                                .post("login")
-                                .then()
-                                .statusCode(302)
-                                .extract()
-                                .cookie("NOPCOMMERCE.AUTH");
+        step("Get cookie and set it to browser by API", () -> {
+            String authorizationCookie = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .when()
+                    .post("login")
+                    .then()
+                    .statusCode(302)
+                    .extract()
+                    .cookie("NOPCOMMERCE.AUTH");
 
-                        step("Open minimal content, because cookie can be set when site is opened", () ->
-                                open("Themes/DefaultClean/Content/images/logo.png"));
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("Themes/DefaultClean/Content/images/logo.png"));
 
-                        step("Set cookie to to browser", () ->
-                                getWebDriver().manage().addCookie(
-                                        new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
-                    });
+            step("Set cookie to to browser", () ->
+                    getWebDriver().manage().addCookie(
+                            new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
+        });
 
-                    step("Refresh page", () ->
-                            refresh());
-                });
+        step("Open user's address", () ->
+                open("customer/addresses"));
 
-            step("Add product to Wishlist", () -> {
-                given()
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .when()
-                        .body(body)
-                        .post("addproducttocart/details/53/2")
-                        .then()
-                        .statusCode(200)
-                        .body("updatetopwishlistsectionhtml", is("(2)"))
-                        .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
-            });
+        step("Check user's address", () -> {
 
-            step("Open wish list", () ->
-                    open("wishlist"));
+            step("Check the address's title", () ->
+                    address.$(".title").shouldHave(text("qa qa")));
 
-            step("Check product in WishList", () ->
-                    $(".product > a").shouldHave(text("3rd Album")));
+            step("Check the user's name", () ->
+                    address.$(".name").shouldHave(text("qa qa")));
 
-            step("Check product quantity in WishList", () ->
-                    $(".qty-input").shouldHave(value("2")));
+            step("Check the email", () ->
+                    address.$(".email").shouldHave(text("Email: elena@qu.guru")));
+
+            step("Check the phone number", () ->
+                    address.$(".phone").shouldHave(text("Phone number: +1234567")));
+
+            step("Check the address", () ->
+                    address.$(".address1").shouldHave(text("Address")));
+
+            step("Check the city, state, zip code", () ->
+                    address.$(".city-state-zip").shouldHave(text("City, Alberta ZipCode")));
+
+            step("Check the country", () ->
+                    address.$(".country").shouldHave(text("Canada")));
+        });
     }
 }
